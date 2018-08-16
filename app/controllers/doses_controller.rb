bfa05588db1989e311e710babdf3329e62fb3c66
thibaultvillard <thibault.villard@gmail.com>
2018-08-16 17:38:24 +0200

    Change of add-dose position

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,18 +1,17 @@
 class DosesController < ApplicationController
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'cocktails/show'
     end
   end
 
@@ -20,7 +19,6 @@ class DosesController < ApplicationController
     @dose = Dose.find(params[:id])
     @cocktail = @dose.cocktail
     @dose.destroy
-
     redirect_to cocktail_path(@cocktail)
   end
 
